5a1e0c017b555f9ecd80ffd92db8a2c56ff37e22
Solomon APPIER-SIGN <braasig@gmail.com>
2022-08-04 02:45:05 +0000

    added realtime model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use AppierSign\RealtimeModel\Traits\RealtimeModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, RealtimeModel;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +42,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function toRealtimeData(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'createdAt' => $this->created_at
+        ];
+    }
+
+    public function getSyncKey(): string
+    {
+        return 'id';
+    }
 }
